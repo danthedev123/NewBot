ca308a5bb023b04fbf0af39a0e40d3f951ed4e67
DanTheDev <54577461+danthedev123@users.noreply.github.com>
2022-12-10 08:18:35 +0400

    .gitignore is now working

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ client.on("ready", () => {
 
     // Put the new c into the API and catch any errors
     try {
-      rest.put(Routes.applicationCommands(""), {
+      rest.put(Routes.applicationCommands("1050285068481986580"), {
         body: c,
       });
     } catch (e) {
